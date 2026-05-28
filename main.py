3a4c3e555b88f7bc58e7f4937c44cb05763d2404
Roman Perelygin <perelyginrv3@gmail.com>
2026-05-28 15:58:22 +1000

    Master: изменено имя переменной result -> total, добавлена multiply с переменной result

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ def power(a, b): return a ** b
 def calculate_sum(numbers):
     return sum(numbers)
 def add(a, b):
-    result = a + b
+    total = a + b
+    return total
+def multiply(a, b):
+    result = a * b
     return result
