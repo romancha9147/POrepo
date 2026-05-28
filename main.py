cef0ab238675240f9e683a502ddcfe966e4dbdc0
Roman Perelygin <perelyginrv3@gmail.com>
2026-05-28 15:50:31 +1000

    Test_two: изменено имя переменной result -> sum_result, добавлена multiply

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ def subtract(a, b): return a - b
 def power(a, b): return a ** b
 def calculate_sum(numbers):
     return sum(numbers)
+def add(a, b):
+    sum_result = a + b
+    return sum_result
+def multiply(a, b):
+    product = a * b
+    return product
